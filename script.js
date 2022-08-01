019175401b2799bb820eeb0beff51fe0eaccd1ff
Jamie Sables <jamie.sables@outlook.com>
2022-08-01 16:33:49 +0100

    started to build out js event listeners

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+/*
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number'
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value)
+*/
+
+
+//Add event listening for Check! button click.
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    if (!guess) {
+        document.querySelector('.message').textContent = 'Please Pick A Number Between 1 & 20'
+    }
+})
