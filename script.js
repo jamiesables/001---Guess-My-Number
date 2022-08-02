75ec863ce9eb36806b905c06c87c4eca59cae43c
Jamie Sables <jamie.sables@outlook.com>
2022-08-02 09:26:15 +0100

    finsihed JS code, some cosmetic changes to html

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
 'use strict';
 
-/*
-console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.message').textContent = 'Correct Number'
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value)
-*/
-
 //Generate random number for player to guess AND setup scoreboard
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
-
+let highScore = 0;
 
 //Add event listening for Check! button click.
 document.querySelector('.check').addEventListener('click', function() {
@@ -26,7 +13,7 @@ document.querySelector('.check').addEventListener('click', function() {
 //If statement to check if there is a guess and if so, is it right?
    
 
-    //When there is no input
+    //When there is no input from user
     if (!guess) {
         document.querySelector('.message').textContent = 'Please Pick A Number Between 1 & 20'
     } 
@@ -34,10 +21,17 @@ document.querySelector('.check').addEventListener('click', function() {
     //When guess is correct
     else if (guess === secretNumber) {
         document.querySelector('.message').textContent = 'Correct Number'
+        document.querySelector('.number').textContent = secretNumber;
+    
         document.querySelector('body').style.backgroundColor = '#60b347'
         document.querySelector('.number').style.width = '30rem'
+
+    //Code to save high score on page
+        if (score > highScore) {
+            highScore = score;
+        }
+        document.querySelector('.highscore').textContent = highScore
     } 
-    
     
     else if (guess > secretNumber) {
 
@@ -70,4 +64,13 @@ document.querySelector('.check').addEventListener('click', function() {
     }
 })
 
-
+//Code to reset eveything when "play again" button is pressed
+document.querySelector('.again').addEventListener('click', function() {
+    document.querySelector('body').style.backgroundColor = '#00008B';
+    document.querySelector('.score').textContent = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.number').textContent = '?'
+    document.querySelector('.guess').value = '';
+})
